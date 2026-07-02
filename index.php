a353d648ad49b7bad661b86f32aaa9fdcb2e1f18
dieynalene2 <dieylene@gmail.com>
2026-07-02 04:09:38 +0000

    product

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+require_once __DIR__ . '/../model/product.model.php';
---
+++ model/product.model.php
@@ -0,0 +1,7 @@
+<?php
+$products = [
+    0=>['ref'=>'ref1','libele'=>'lib1','prix'=>2000,'quantite'=>12],
+    1=>['ref'=>'ref2','libele'=>'lib2','prix'=>500,'quantite'=>2],
+];
+
+$productsArchived = [];
