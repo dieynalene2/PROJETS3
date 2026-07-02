b962534693f318c0538e9728a4668951f8591b9e
dieynalene2 <dieylene@gmail.com>
2026-07-02 04:24:24 +0000

    gererrf

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ require_once __DIR__ . '/../model/commande.model.php';
 require_once __DIR__ . '/../utils/error.php';
 require_once __DIR__ . '/../utils/validator.php';
 require_once __DIR__ . '/../utils/view.utils.php';
+require_once __DIR__ . '/../utils/sevice.php';
---
+++ service/service.php
@@ -0,0 +1,26 @@
+<?php
+
+function genererReference( array $products):string{
+    $taille=count($products)+1;
+     if($taille<=9){
+        $ref="REF00";
+    }elseif ($taille<=99){
+        $ref="REF0";
+    }else{
+        $ref="REF";
+    }
+    return $ref.$taille;
+}
+
+function getProductByLibele (array $products, string $value): int{
+    foreach ($products as $index => $product) {
+                    if ($product["libele"] == $value){
+                    return $index;
+                }
+            }
+            return -1 ;
+}
+
+function supprimerProduit (int $index, array &$products): array {
+        return array_splice($products, $index, 1)[0];
+}
