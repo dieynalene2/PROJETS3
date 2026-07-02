053fdbf0ab5e943b811e03b56c1a5e25fb9b20be
dieynalene2 <dieylene@gmail.com>
2026-07-02 04:21:04 +0000

    messa

--- index.php
+++ index.php
@@ -4,3 +4,4 @@ require_once __DIR__ . '/../model/client.model.php';
 require_once __DIR__ . '/../model/commande.model.php';
 require_once __DIR__ . '/../utils/error.php';
 require_once __DIR__ . '/../utils/validator.php';
+require_once __DIR__ . '/../utils/view.utils.php';
