bf95ea888cc5f87c9b792d475360101742b80428
dieynalene2 <dieylene@gmail.com>
2026-07-02 04:18:51 +0000

    mess

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ require_once __DIR__ . '/../model/product.model.php';
 require_once __DIR__ . '/../model/client.model.php';
 require_once __DIR__ . '/../model/commande.model.php';
 require_once __DIR__ . '/../utils/error.php';
+require_once __DIR__ . '/../utils/validator.php';
